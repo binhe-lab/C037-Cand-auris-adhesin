5b671940b558f23e315a10f7b152306330a521de
Bin He <emptyhb@gmail.com>
2020-02-10 08:44:47 -0600

    added FungalRV script and compiled the source c code except for one

---
+++ 01-global-adhesin-prediction/script/FungalRV_adhesin_predictor/calc_aafreq.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+main(int argc, char *argv[])
+{
+char *geneline;
+char *sequence;
+char aacode[20] = {'A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y'};
+char c;
+int i,j,index,len;
+int bool;
+float aafreq,aacount[20];
+char infile[50], outfile[50];
+int proteincount = 0,processedcount = 0;
+FILE *f_faa,*f_write;
+if(argc!=3)
+	{
+		if(argc!=2)
+		{
+			printf("Enter the inputfile: ");
+			scanf("%s",infile);
+		}
+		else
+
+			strcpy(infile,argv[1]);
+		printf("Enter the outputfile: ");
+		scanf("%s",outfile);
+	}
+	else
+	{
+		strcpy(infile,argv[1]);
+		strcpy(outfile,argv[2]);
+	}
+	//printf("Enter the outputfilename: ");
+	//scanf("%s",outputfile);
+
+	f_faa=fopen(infile,"r");
+	if(f_faa==NULL)
+	{
+		printf("File %s not found.\n",infile);
+		exit(3);
+	}
+
+	f_write=fopen(outfile,"w");
+	if(f_write==NULL)
+	{
+		printf("Cannot open file %s to write.\n",outfile);
+		exit(4);
+	}
+while((c = getc(f_faa)) != EOF)
+	{
+	if(c != '>') continue;
+	geneline = malloc(0);
+	i = 0;
+		while(((c = getc(f_faa)) != '\n') && (c != '\r') && (c != '\f'))
+		{
+			geneline = realloc(geneline,++i);
+			geneline[i - 1] = c;
+		}
+	geneline = realloc(geneline,++i);
+	geneline[i - 1] = '\0';
+	sequence = malloc(0);
+	len = 0;
+	bool = 1;
+	proteincount++;
+		while(((c = getc(f_faa)) != '>') && (c != EOF) && (bool))
+		{
+			if(!isalpha(c)) continue;
+			if((c == 'B') || (c == 'J') || (c == 'O') || (c == 'U') || (c == 'X') || (c == 'Z'))
+			bool = 0;
+			sequence = realloc(sequence,++len);
+			sequence[len-1] = c;
+		}
+		if(bool == 0)
+		{
+			fprintf(f_write,"Sequence ambiguity : %c Found in %s\nEntry Skipped\n",sequence[len - 1],geneline);
+			continue;
+		}
+	for(i = 0; i < 20; ++i)
+	aacount[i] = 0;
+	processedcount++;
+	for(i = 0; i < len; ++i)
+		{
+			for(j = 0; ((j < 20) && (sequence[i] != aacode[j])); ++j);
+			index = j;
+			aacount[index]++;
+		}
+//fprintf(f_write,"0\t");
+		for(i = 0; i < 20; i++)
+		{
+			aafreq = (float)((aacount[i])/len);
+			if(aafreq>0)
+			fprintf(f_write,"%d:%.8f\t",i+3921,aafreq);
+		}
+	fprintf(f_write,"\n");
+	fseek(f_faa,-2L,1);
+}
+
+printf("\nTotal Proteins in %s : %d","NC_000964.faa",proteincount);
+printf("\nTotal Proteins Processed (Proteins Without Sequence Ambiguity Like BJOUXZ): %d\n",processedcount);
+fclose(f_faa);
+fclose(f_write);
+}
---
+++ 01-global-adhesin-prediction/script/README.md
@@ -0,0 +1,14 @@
+# List of scripts
+| Item | Description | Source | Date |
+| ---- | ----------- | ------ | ---- |
+| extract_fasta.sh | shell script to extract fasta sequences based on a list of IDs | HB, custom | 2020-02-04 |
+| FungalRV adhesin predictor | tools for predicting adhesins | http://fungalrv.igib.res.in/download.html | 2020-02-09 |
+
+# Notes
+## 2020-02-10 Compiled and tested FungalRV code
+Used the following command to compile the c files on my Ubuntu. Should be similar on other machines, too. If the binaries don't work on other systems, follow my example to compile the source c code.
+```bash
+# I assume you are in the FungalRV script folder, where the c source files are
+for f in *.c;do gcc $f -o ./bin/${f/.c/}; done
+```
+Note that `FungalRV_adhesin_predictor/calc_hdr_comp.c` failed to compile.
