3a1b456fddbf10811f6314a61cf2e2e8e8418322
Bin He <emptyhb@gmail.com>
2021-05-07 10:35:22 -0500

    Cauris tandem repeat analyses done

---
+++ 02-case-studies/analysis/Cauris-polymorphism/output/tandem-repeats/README.md
@@ -0,0 +1,6 @@
+tandem repeat prediction results by [XSTREAM](https://amnewmanlab.stanford.edu/xstream/)
+
+usage: 
+java -Xmx1000m -Xms1000m -jar ~/sw/XSTREAM/xstream.jar ../../input/cauris-five-strains-for-gene-tree.fasta -i.7 -I.7 -g3 -e2 -z -O -B -aName
+
+See user manual for details of the parameters
---
+++ 02-case-studies/analysis/Cauris-polymorphism/script/extract_fasta.py
@@ -0,0 +1,32 @@
+"""Extract the subset of fasta sequences given the sequence names
+   Bin He
+   2020-06-06
+"""
+
+# import libraries
+from Bio import SeqIO
+import sys
+import re
+
+# parse arguments
+if len(sys.argv) != 3: # the first element is the program name
+    print("python3 extract_fasta.py <FASTA> <SEQ NAMES>")
+    sys.exit()
+
+FA = sys.argv[1] # Fasta file
+IN = sys.argv[2] # Seq names
+OUT = re.sub("\.txt",".faa", IN)
+
+# read in fasta and turn it into a dictionary https://biopython.org/wiki/SeqIO
+with open(FA, "r") as FH:
+    fasta_records = SeqIO.to_dict(SeqIO.parse(FH, "fasta"))
+
+# create a container for filtered results
+filtered = []
+
+# iterate through the list of seq names
+with open(IN, "r") as f:
+    for name in f:
+        filtered.append(fasta_records[name.rstrip()])
+
+SeqIO.write(filtered, OUT, "fasta")
