45f34cf3eb783506151bdd18633480deab1e1eb4
Bin He <emptyhb@gmail.com>
2021-07-02 14:23:54 -0500

    finished figure 5, 6

--- 02-case-studies/04-homolog-properties/2020-10-31/script/S-T-freq.sh
+++ 02-case-studies/04-homolog-properties/2020-10-31/script/S-T-freq.sh
@@ -2,18 +2,17 @@
 # author: Bin He
 # date: 2021-06-28
 
-path=../data/proteome-fasta/
 files=(
-"C_albicans_GCF_000182965.3_ASM18296v3_protein.faa.gz"
-"C_glabrata_CBS138_new_genome_release_20200224_cds.fa.gz"
-"GCF_002775015.1_Cand_auris_B11221_V1_protein.faa.gz"
-"S_cerevisiae_GCF_000146045.2_R64_protein.faa.gz"
+"../data/proteome-fasta/C_albicans_GCF_000182965.3_ASM18296v3_protein.faa.gz"
+"../data/proteome-fasta/C_glabrata_GCA_010111755.1_ASM1011175v1_protein.faa.gz"
+"../data/proteome-fasta/GCF_002775015.1_Cand_auris_B11221_V1_protein.faa.gz"
+"../data/proteome-fasta/S_cerevisiae_GCF_000146045.2_R64_protein.faa.gz"
 )
 names=("Calbicans" "Cglabrata" "Cauris" "Scerevisiae")
 
 # https://unix.stackexchange.com/questions/278502/accessing-array-index-variable-from-bash-shell-script-loop
 for ((idx=0; idx<${#files[@]}; ++idx)); do
 	echo "Working on ${files[idx]}..."
-	python ../script/calc_aafreq_gz.py $path/${files[idx]} > ../output/ST-freq/${names[idx]}-ST-freq.tsv
+	python ../script/calc_aafreq_gz.py ${files[idx]} | gzip -c > ../output/ST-freq/${names[idx]}-ST-freq.tsv.gz
 	echo "done."
 done
