349b23a1e2e446c93b5ec514a4f3bf85806320f4
Bin He <emptyhb@gmail.com>
2022-05-16 09:20:20 -0500

    finished generax first run

--- 02-case-studies/03-gene-tree/20220512-raxmlng-expanded/script/generax-clustalo.sh
+++ 02-case-studies/03-gene-tree/20220512-raxmlng-expanded/script/generax-clustalo.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+############################
 # title: run GeneRax to "correct" the gene tree by reconciling it with the species tree using ML
 # author: Bin He
 # date: 2021-12-07
+# use: qsub generax-clustalo.sh
+#----------------------
+# scheduler parameters
+#$ -q BH,BIO-INSTR
+#$ -M bhe2@uiowa.edu
+#$ -m ea
+#$ -pe smp 56
+#$ -N generax-clustalo
+#$ -cwd
+#$ -o job-log/$JOB_NAME_$JOB_ID.out
+#$ -e job-log/$JOB_NAME_$JOB_ID.err
+#----------------------
+# -m ea will email the 
+#    user when the job
+#    ends or aborts
+########################
 
-# all input files for the analysis is included in this folder to keep things organized
-# this script file is meant to be run by copy-n-pasting the individual commands to the command line and run individually
+# these are useful flags to set to make the code more robust to failure
+# copied from Vince Buffalo's Bioinformatic Data Analysis book
+set -e
+set -u
+set -o pipefail
 
 # 2021-12-07 uses the recommended radius = 5, and used the parallel version
-mpiexec -np 2 generax --families families-clustalo.txt --species-tree generax/species-tree.nwk --rec-model UndatedDL --per-family-rates --prefix ../output/generax --max-spr-radius 5
+mpiexec -np 56 generax --families families-clustalo.txt --species-tree generax/species-tree.nwk --rec-model UndatedDL --per-family-rates --prefix ../output/generax --max-spr-radius 5
