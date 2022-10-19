ac7285e6622d420e4b2694ec5f20eb6c2c7a3e58
Bin He <emptyhb@gmail.com>
2022-10-19 09:06:34 -0500

    gene tree using VT+I+G model based on modeltest

---
+++ 02-case-studies/03-gene-tree/20220512-raxmlng-expanded/script/raxml-ng-modeltest.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+############################
+# author: Bin He
+# date: 2022-05-13
+# title: RAxML-ng tree search
+# use: qsub raxmlng.sh
+#----------------------
+# scheduler parameters
+#$ -q BH,BIO-INSTR
+#$ -M bhe2@uiowa.edu
+#$ -m ea
+#$ -pe smp 56
+#$ -N raxml-ng
+#$ -cwd
+#$ -o job-log/$JOB_NAME_$JOB_ID.out
+#$ -e job-log/$JOB_NAME_$JOB_ID.err
+#----------------------
+# -m ea will email the 
+#    user when the job
+#    ends or aborts
+########################
+
+# these are useful flags to set to make the code more robust to failure
+# copied from Vince Buffalo's Bioinformatic Data Analysis book
+set -e
+set -u
+set -o pipefail
+
+# set input and intermediate file names
+#base=expanded-blast-PF11765_hmmalign_clipkit
+base=expanded-blast-PF11765_clustalo_clipkit
+#base=expanded-blast-PF11765_clustalo
+align=../output/align/${base}.faa
+out=../output/raxml-ng/${base}_VT_I_G4
+
+# estimate the tree
+/Users/bhe2/bin/raxml-ng-mpi --all --msa $align --model VT+I+G4 --seed 123 --threads auto --workers auto --bs-trees autoMRE --prefix $out
